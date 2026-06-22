b1f46af6e29af0650ec180ada20bd24504af35d5
priyanshunagle14 <priyanshunagle2020@gmail.com>
2026-06-23 00:27:25 +0530

    fix API route

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "CodeVector API is running 🚀",
+    status: "ok",
+    endpoints: {
+      products: "/api/products"
+    }
+  });
+});
 app.use("/api/products", productRoutes);
 
 module.exports = app;
